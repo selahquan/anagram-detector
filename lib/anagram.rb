98c9922ecf4dd1d7c3c1db0e105b2a20e90fce65
Josh <josh.handon@gmail.com>
2020-11-25 20:08:15 -0500

    commit tree 2984362c9208150c149cad249c6452633057f5ff

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(anagram_arr)
+        anagram_arr.select do |item|
+            @word.split("").sort == item.split("").sort
+            end
+        end
+    end
+end
